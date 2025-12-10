8414e7a7143e07836bd915c0e8204e329e7c1337
Julian Nguyen <huutrong.160705@gmail.com>
2025-12-10 20:37:32 +0700

    update BE

--- BE_MoneyTrackBot/myproject/wsgi.py
+++ BE_MoneyTrackBot/myproject/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for myproject.
-
-This module exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information, see:
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
